85482f98240ba2ddf810ceda75cb5252043597fe
Pedro <ph@educreational.com>
2013-06-21 15:24:51 -0400

    Adding touch support

--- drawing_app/js/app/StrokeRecorder.js
+++ drawing_app/js/app/StrokeRecorder.js
@@ -95,6 +95,55 @@ StrokeRecorder.prototype.init = function() {
         strokeEnd();
     };
 
+    if (true) {
+        var context = canvas.getContext('2d');
+
+        // create a drawer which tracks touch movements
+        var drawer = {
+            isDrawing: false,
+            touchstart: function(coors){
+                context.beginPath();
+                context.moveTo(coors.x, coors.y);
+                this.isDrawing = true;
+            },
+            touchmove: function(coors){
+                if (this.isDrawing) {
+                    context.lineTo(coors.x, coors.y);
+                    context.stroke();
+                }
+            },
+            touchend: function(coors){
+                if (this.isDrawing) {
+                    this.touchmove(coors);
+                    this.isDrawing = false;
+                }
+            }
+        };
+        // create a function to pass touch events and coordinates to drawer
+        function draw(event){
+            // get the touch coordinates
+            var coors = {
+                x: event.targetTouches[0].pageX,
+                y: event.targetTouches[0].pageY
+            };
+            // pass the coordinates to the appropriate handler
+            drawer[event.type](coors);
+        }
+        
+        // attach the touchstart, touchmove, touchend event listeners.
+        canvas.addEventListener('touchstart',draw, false);
+        canvas.addEventListener('touchmove',draw, false);
+        canvas.addEventListener('touchend',draw, false);
+        
+        // prevent elastic scrolling
+        document.body.addEventListener('touchmove',function(event){
+            event.preventDefault();
+        },false);   // end body.onTouchMove
+    }
+
+
+
+/*
     canvas.touchstart = function(event) {
         event.preventDefault();
 
@@ -133,7 +182,7 @@ StrokeRecorder.prototype.init = function() {
     canvas.touchleave = function(event) {
         strokeCancel();
     };
-
+*/
 
 	// With jquery, we can get rid of a lot of this "boilerplate" code
 
